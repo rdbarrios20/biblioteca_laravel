37150a89d0e8b011efb19234ab0439d704e9e2ab
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-20 22:26:14 -0500

    Add table styles and structure

--- resources/views/bitacora.blade.php
+++ resources/views/bitacora.blade.php
@@ -1,86 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        @include('partial.head');
     </head>
     <body>
     <h1>{{ $response }}</h1>
-    @foreach ($bitacora as $item)
-        <table>
-            <thead>
-
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->rol }}</td>
-                    <td>{{ $item->id_usuario }}</td>
-                    <td>{{ $item->accion }}</td>
-                    <td>{{ $item->fecha }}</td>
-                    <td>{{ $item->detalle }}</td>
-                </tr>
-            </tbody>
-        </table>
-    @endforeach
+        <div class="background">
+            <br>
+            <div class="busqueda">
+                <label for="fecha">Limpiar por fecha</label>
+                <input type="date" name="fecha" id="fecha" class="">
+                <span></span>
+                <button type="button" class="btn btn-danger"  value="limpiar" data-toggle="modal" data-target="#Eliminar_fecha"><span class='icon-trash'></span></button>
+            </div>
+            <div class="contenedor">
+                <div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Rol</th>
+                                <th>Id Usuario</th>
+                                <th>Accion</th>
+                                <th>Fecha</th>
+                                <th>Detalle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($bitacora as $item)
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>{{ $item->rol }}</td>
+                                <td>{{ $item->id_usuario }}</td>
+                                <td>{{ $item->accion }}</td>
+                                <td>{{ $item->fecha }}</td>
+                                <td>{{ $item->detalle }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
