b58232957ef873325b1c7b5555c514659bc8a63c
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-20 22:27:40 -0500

    Add header reusable

---
+++ resources/views/partial/head.blade.php
@@ -0,0 +1,9 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Estilos/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/style.css')}}">
+
+    <script src="Scripts/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="Scripts/bitacora.js"></script>
