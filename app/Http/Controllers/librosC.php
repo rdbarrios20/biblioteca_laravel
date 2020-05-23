77af3263450fa34dba9a19ee97afb8b598d28732
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-22 21:40:25 -0500

    Creating view inventario

--- app/Http/Controllers/librosC.php
+++ app/Http/Controllers/librosC.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Libros;
 use Illuminate\Http\Request;
 
 class LibrosC extends Controller
 {
     //
+    public function LibrosView(){
+        $data = Libros::get();
+        $libros = [
+            'response'=>'Hello World'
+        ];
+        return view('inventario')->with($libros);
+    }
 }
