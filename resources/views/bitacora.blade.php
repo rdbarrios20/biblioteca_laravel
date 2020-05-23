6fc1c68cfb0e9626502de5ba74dab9568d42ba68
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-22 20:31:03 -0500

    Add page tittle

--- resources/views/bitacora.blade.php
+++ resources/views/bitacora.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('partial.head');
+        <title>Bitacora</title>
     </head>
     <body>
         <header>
