2033a2e4382f297d1707fa661ccb19f16b1c7470
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-28 23:10:15 -0500

    Script for require dates from DB

--- app/Http/Controllers/loginC.php
+++ app/Http/Controllers/loginC.php
@@ -8,11 +8,49 @@ use Illuminate\Http\Request;
 class loginC extends Controller
 {
     //
-    public function loginvalidate(Request $request){
-        $data = usuarios::get();
-        $datos = [
-            'list'=>$data
-        ];
-        return view('login')->with($datos);
+    public function loginView(Request $request){
+        return view('login');
+    }
+
+    public function loginValidate(Request $request){
+        // <var></var>
+            // dd($request);
+            $dbResponseLs = usuarios::where('usuario','=', $request->user)->limit(1)->get();
+            //dd($dbResponseLs);
+            if( count($dbResponseLs) > 0){
+                //quiere decir que encobntro el dato
+
+                //ahora validamos el passowrd
+
+                // dd($dbResponseLs[0]);
+                //dd($dbResponseLs[0]->usuario);
+                
+                if($request->user == $dbResponseLs[0]->usuario 
+                   && $request->pasword == $dbResponseLs[0]->password)
+                {   
+                    //Creo session terminal en php
+                    session('USUARIO_LOGGEADO', true);
+
+                    $response = [];
+                    $response['validacion'] = true;
+                    $response['tipo'] = 'Administrador';
+
+                    return response()->json($response);
+                    dd($response);
+                }else{
+                    // var_dump('HELLO');
+                    return response()->json('Datos incorrectos');
+                }
+            }else{
+                // var_dump('HELLO');
+                return response()->json('EL usauriio no existe');
+            }
+
+
+            // $data = usuarios::get();
+            // $datos = [
+            //     'list'=>$data
+            // ];
+            // return view('login')->with($datos);
     }
 }
