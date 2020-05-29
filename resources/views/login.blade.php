fd8010157c4cba14bf98465a6e008d1a11cdcb21
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-05-28 23:18:26 -0500

    Redifine the file structure

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     @include('partial.head')
+    <script src="{{asset('Scripts/login.js')}}"></script>
 </head>
 <body>
+    <div class="error">
+        <span>Los datos ingresados son errados por favor verifique nuevamente</span>
+    </div>
     <section>
         <div class="container">
             <div id="login-row" class="row justify-content-center aling-items-center">
@@ -14,18 +18,9 @@
                     <div class="banner-top">
                         <h3>Biblioteca Virtual</h3>
                         <div id="login-box" class="col-md-12">
-                            @foreach ($list as $item)
-                            
-                                {{$item ->id_usuario}}
-                                {{$item ->usuario}}
-                                {{$item ->password}}
-                                {{$item ->identificacion}}
-                                {{$item ->id_rol}}
-                                {{$item ->id_rol_secundario}}
 
-                            @endforeach
-
-                            <form action="" class="login-form">
+                            {{-- <form action="/login-validate" method='POST' class="login-form"> --}}
+                            <form id='login-form' class="login-form">
                                 @csrf
                                 <div class="form-group">
                                     <label for="">Usuario:</label>
@@ -34,7 +29,7 @@
                                             <span class="input-group-text icon-users"></span>
                                         </div>
                                         <input type="text" name="user" pattern="[A-Za-z0-9_-]{1,15}" 
-                                        class="form-control" placeholder="Usuario" value="Rafael" required>
+                                        class="form-control" placeholder="Usuario" value="rafa" required>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -44,7 +39,7 @@
                                             <span class="input-group-text icon-key"></span>
                                         </div>
                                         <input type="password" name="pasword" pattern="[A-Za-z0-9]{1,15}" class="form-control"
-                                         placeholder="****************" value="1065632580" required>
+                                         placeholder="****************" value="rafa" required>
                                     </div>
                                 </div>
                                 <div class="form-group button">
