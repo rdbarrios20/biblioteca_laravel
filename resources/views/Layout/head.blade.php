c5bdbeaeb5a9b3f9f762f1ca7998cfb87c55f3d6
Rafael Barrios - Laptop <ralfhacker.91@hotmail.com>
2020-05-31 15:14:23 -0500

    Rename folder

---
+++ resources/views/Layout/head.blade.php
@@ -0,0 +1,10 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('Estilos/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('Estilos/login.css')}}">
+    <link rel="stylesheet" href="{{asset('fonts/loader.css')}}">
+
+    <script src="{{asset('Scripts/jquery-3.4.1.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('Scripts/bitacora.js')}}"></script>
