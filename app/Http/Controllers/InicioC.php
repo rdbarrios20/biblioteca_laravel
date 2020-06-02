45770a0dcd303628457ba89283bb4a664422f202
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-06-01 23:15:54 -0500

    Reconfigured inicio view

--- app/Http/Controllers/InicioC.php
+++ app/Http/Controllers/InicioC.php
@@ -8,6 +8,14 @@ class InicioC extends Controller
 {
     //
     public function InicioView(Request $request){
-        return view('inicio');
+
+        $userIsLogged = $request->session()->get('USUARIO_LOGEADO',true);
+
+        dd($userIsLogged);
+        if($userIsLogged){
+            return view('inicio');
+        }else{
+            return view('login');
+        }
     }
 }
