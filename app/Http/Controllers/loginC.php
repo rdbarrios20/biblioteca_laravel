eb23b1aa6bbd7fe29ff8269a6537a524239c0ebf
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-06-02 19:46:49 -0500

    Validate session on login

--- app/Http/Controllers/loginC.php
+++ app/Http/Controllers/loginC.php
@@ -8,42 +8,57 @@ use Illuminate\Http\Request;
 class loginC extends Controller
 {
     //
-    public function loginView(Request $request){
-        return view('login');
+    public function loginView(Request $request)
+    {
+        if ($request->session()->exists('USUARIO_LOGGEADO')) {
+
+            $usserIsLogged = $request->session()->get('USUARIO_LOGGEADO');
+
+            if ($usserIsLogged === 'administrador') {
+                // return view('inicio');
+                return redirect('inicio');
+            } else {
+                //Objectivo
+                return view('login');
+            }
+        } else {
+            return view('login');
+        }
     }
 
-    public function loginValidate(Request $request){
-            // dd($request);
-            $dbResponseLs = usuarios::where('usuario','=', $request->user)->limit(1)->get();
-            //dd($dbResponseLs);
-            if( count($dbResponseLs) > 0){
-                //quiere decir que encobntro el dato
 
-                //ahora validamos el passowrd
+    ///1. Crear ruta en web.config
+    //2. Crear funcion que manja la ruta
+    //3. DEstro de la funcion destruir todas las session creadas en el servidor
+    //4. redireccionar al login..
 
-                // dd($dbResponseLs[0]);
-                //dd($dbResponseLs[0]->usuario);
-                
-                if($request->user == $dbResponseLs[0]->usuario 
-                   && $request->pasword == $dbResponseLs[0]->password)
-                {   
-                    //Creo session en el servidor 
-                    $request->session()->put('USUARIO_LOGGEADO', true);
+    public function loginValidate(Request $request)
+    {
+        $dbResponseLs = usuarios::where('usuario', '=', $request->user)->limit(1)->get();
 
-                    $response = [];
-                    $response['validacion'] = true;
-                    $response['tipo'] = 'Administrador';
+        if (count($dbResponseLs) > 0) {
 
-                    //  dd($response);
-                    return response()->json($response);
-                }else{
-                    // var_dump('HELLO');
-                    return response()->json('Datos incorrectos');
-                }
-            }else{
+            if (
+                $request->user == $dbResponseLs[0]->usuario
+                && $request->pasword == $dbResponseLs[0]->password
+            ) {
+                //Creo session en el servidor 
+                $request->session()->put('USUARIO_LOGGEADO', 'administrador');
+                //$request->session()->put('USUARIO_ROL', 'ADMINISTRADOR');
+
+                $response = [];
+                $response['validacion'] = true;
+                $response['tipo'] = 'Administrador';
+
+                // return redirect('/inicio');
+                return response()->json($response);
+            } else {
                 // var_dump('HELLO');
-                return response()->json('EL usauriio no existe');
+                return response()->json('Datos incorrectos');
             }
-
+        } else {
+            // var_dump('HELLO');
+            return response()->json('EL usauriio no existe');
+        }
     }
 }
