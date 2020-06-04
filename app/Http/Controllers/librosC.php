708ce807420df0b87baaeef83cf4afec5186de6e
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-06-04 17:50:20 -0500

    Insert libros function and libros view

--- app/Http/Controllers/librosC.php
+++ app/Http/Controllers/librosC.php
@@ -8,11 +8,21 @@ use Illuminate\Http\Request;
 class LibrosC extends Controller
 {
     //
-    public function LibrosView(){
+
+    public function InsertLibros(Request $request){
+        
+    }
+
+    public function LibrosView(Request $request){
+        return view('libros');
+    }
+
+    public function InventarioView(){
         $data = Libros::get();
         $libros = [
             'libros'=>$data
         ];
         return view('inventario')->with($libros);
     }
+
 }
