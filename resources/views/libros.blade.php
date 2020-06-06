06313a005a5ae2d2f78bdcabeb2096f566a114f8
Rafael Barrios - Laptop <ralfhacker@hotmail.com>
2020-06-05 22:07:30 -0500

    Creating select for categoria

--- resources/views/libros.blade.php
+++ resources/views/libros.blade.php
@@ -74,10 +74,59 @@
                     <label for="">Cantidad</label>
                     <input type="text" name="cantidad" id="cantidad" class="form-control" required>
                 </div>
+                <div class="form-group">
+                    <label for="">Categoria</label>
+                    <select name="id_categoria" id="id_categoria">
+                        <option value="1">Romanticos</option>
+                        <option value="2">Ficcion</option>
+                        <option value="3">Familia</option>
+                    </select>
+                </div>
                 <div class="form-group cold-md3">
                     <input type="submit" id="btenviar" class="btn btn-lg btn-success" value="Guardar">
                 </div>
             </form>
+        </div>
+        <!--Modal para el mensaje de datos guardados satisfactoriamente -->
+        <div>
+            <div class="modal" tabindex="-1" role="dialog" id="success_message">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Mensaje Datos Guardados</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p id="message"></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-info" data-dismiss="modal">Aceptar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal para el validar los precios-->
+        <div class="modal" tabindex="-1" role="dialog" id="Cost_message">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Validacion Precios</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p id="Cost"></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-info" data-dismiss="modal">Aceptar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </body>
